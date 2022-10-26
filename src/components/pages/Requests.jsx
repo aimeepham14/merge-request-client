b7d4455a3cf4e872979a4a8cc3de56b90220c985
syorn96 <syorn2014@gmail.com>
2022-10-26 14:13:28 -0700

    updates requests, only shows when user is logged in, updates state in register, default planguage is python

--- src/components/pages/Requests.jsx
+++ src/components/pages/Requests.jsx
@@ -2,10 +2,13 @@ import { useState } from 'react'
 import RequestsChatDisplay from '../RequestsChatDisplay'
 import RequestsDisplay from '../RequestsDisplay'
 import RequestsHeader from '../RequestsHeader'
+import { Link } from 'react-router-dom'
 
-export default function Requests(currentUser) {
+
+export default function Requests({currentUser}) {
     const [ selectedUser, setSelectedUser ] = useState(null)
-    return (
+
+    const Requests = (
         <div style={{backgroundColor: 'black', color:'white'}}>
             <RequestsHeader/>
             <div>
@@ -15,6 +18,22 @@ export default function Requests(currentUser) {
             <RequestsDisplay/>
 
             <RequestsChatDisplay/>
+        </div>
+    )
+
+    const loginMessage = (
+        <div>
+            <p className='text-3xl font-bold mt-20'>Log in chat with other developers!</p>
+            <Link to='/login'>
+                <button className="block w-full rounded-lg bg-indigo-600 px-5 py-3 text-2xl text-yellow font-code">
+                    Log in
+                </button>
+            </Link>
+        </div>
+    )
+    return (
+        <div>
+            {currentUser ? Requests : loginMessage}
         </div>
     )
 }
