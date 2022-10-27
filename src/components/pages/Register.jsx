55fc1e6e6d13de6d67cbd853d35258a0e49587a5
aimeepham14 <aimee.pham14@gmail.com>
2022-10-27 10:51:06 -0700

    fixed city field bug

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -322,71 +322,71 @@ export default function Register({ currentUser, setCurrentUser }) {
 
 
 
-		<section class=" py-1 bg-blueGray-50">
-		<div class="w-full lg:w-8/12 px-4 mx-auto mt-6">
-		<div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
-			<div class="rounded-t bg-white mb-0 px-6 py-6">
-			<div class="text-center flex justify-between">
-				<h6 class="text-blueGray-700 text-3xl font-bold text-code">
+		<section className=" py-1 bg-blueGray-50">
+		<div className="w-full lg:w-8/12 px-4 mx-auto mt-6">
+		<div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
+			<div className="rounded-t bg-white mb-0 px-6 py-6">
+			<div className="text-center flex justify-between">
+				<h6 className="text-blueGray-700 text-3xl font-bold text-code">
 				Sign up to send a merge request...
 				</h6>
 			</div>
 			</div>
-			<div class="flex-auto px-4 lg:px-10 py-10 pt-0">
+			<div className="flex-auto px-4 lg:px-10 py-10 pt-0">
 			<form onSubmit={handleSubmit}>
-				<h6 class="text-blueGray-400 text-2xl mt-3 mb-6 font-bold uppercase text-code">
+				<h6 className="text-blueGray-400 text-2xl mt-3 mb-6 font-bold uppercase text-code">
 				User Information
 				</h6>
-				<div class="flex flex-wrap">
-				<div class="w-full lg:w-6/12 px-4">
-					<div class="relative w-full mb-3">
-					<label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="email">
+				<div className="flex flex-wrap">
+				<div className="w-full lg:w-6/12 px-4">
+					<div className="relative w-full mb-3">
+					<label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="email">
 						Email
 					</label>
-					<input type="email" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value={email} id="email" onChange={e => setEmail(e.target.value)} />
+					<input type="email" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value={email} id="email" onChange={e => setEmail(e.target.value)} />
 					</div>
 				</div>
-				<div class="w-full lg:w-6/12 px-4">
-					<div class="relative w-full mb-3">
-					<label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="password">
+				<div className="w-full lg:w-6/12 px-4">
+					<div className="relative w-full mb-3">
+					<label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="password">
 						Password
 					</label>
-					<input type="password" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value={password} id="password" onChange={e => setPassword(e.target.value)} />
+					<input type="password" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value={password} id="password" onChange={e => setPassword(e.target.value)} />
 					</div>
 				</div>
 
-				<div class="w-full lg:w-4/12 px-4">
-					<div class="relative w-full mb-3">
-					<label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="firstName">
+				<div className="w-full lg:w-4/12 px-4">
+					<div className="relative w-full mb-3">
+					<label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="firstName">
 						First Name
 					</label>
-					<input type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value={firstName} id="firstName" onChange={e => setFirstName(e.target.value)} />
+					<input type="text" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value={firstName} id="firstName" onChange={e => setFirstName(e.target.value)} />
 					</div>
 				</div>
-				<div class="w-full lg:w-4/12 px-4">
-					<div class="relative w-full mb-3">
+				<div className="w-full lg:w-4/12 px-4">
+					<div className="relative w-full mb-3">
 					<label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="lastName">
 						Last Name
 					</label>
-					<input type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value={lastName} id="lastName" onChange={e => setLastName(e.target.value)} />
+					<input type="text" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value={lastName} id="lastName" onChange={e => setLastName(e.target.value)} />
 					</div>
 				</div>
-				<div class="w-full lg:w-4/12 px-4">
-					<div class="relative w-full mb-3">
-					<label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="city">
-						City, State
+				<div className="w-full lg:w-4/12 px-4">
+					<div className="relative w-full mb-3">
+					<label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="city">
+						City
 					</label>
-					<input type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value={city} id="city" onChange={e => setCity(e.target.value)} />
+					<input type="text" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value={city} id="city" onChange={e => setCity(e.target.value)} />
 					</div>
 				</div>
 
 
-				<div class="w-full lg:w-4/12 px-4">
-					<div class="relative w-full mb-3">
-					<label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" for="birthMonth">
+				<div className="w-full lg:w-4/12 px-4">
+					<div className="relative w-full mb-3">
+					<label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="birthMonth">
 						Birth Month
 					</label>
-					<select class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value={birthMonth} required id="birthMonth" name="month" onChange={e => setBirthMonth(e.target.value)}>
+					<select className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value={birthMonth} required id="birthMonth" name="month" onChange={e => setBirthMonth(e.target.value)}>
 							<option value="1">January</option>
 							<option value="2">February</option>
 							<option value="3">March</option>
@@ -403,12 +403,12 @@ export default function Register({ currentUser, setCurrentUser }) {
 					</div>
 				</div>
 
-				<div class="w-full lg:w-4/12 px-4">
-					<div class="relative w-full mb-3">
-					<label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" for="birthDay">
+				<div className="w-full lg:w-4/12 px-4">
+					<div className="relative w-full mb-3">
+					<label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="birthDay">
 						Birth Day
 					</label>
-					<select class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value={birthDay} required id="birthDay" onChange={e => setBirthDay(e.target.value)}>
+					<select className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value={birthDay} required id="birthDay" onChange={e => setBirthDay(e.target.value)}>
 							<option value="1">1</option>
 							<option value="2">2</option>
 							<option value="3">3</option>
@@ -444,12 +444,12 @@ export default function Register({ currentUser, setCurrentUser }) {
 					</div>
 				</div>
 
-				<div class="w-full lg:w-4/12 px-4">
-					<div class="relative w-full mb-3">
-					<label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" for="birthYear">
+				<div className="w-full lg:w-4/12 px-4">
+					<div className="relative w-full mb-3">
+					<label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="birthYear">
 						Birth Year
 					</label>
-					<select class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value={birthYear} required id="birthYear" onChange={e => setBirthYear(e.target.value)}>
+					<select className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value={birthYear} required id="birthYear" onChange={e => setBirthYear(e.target.value)}>
 						{birthYearOptions}
 					</select>
 					</div>
@@ -458,49 +458,49 @@ export default function Register({ currentUser, setCurrentUser }) {
 
 				</div>
 
-				<hr class="mt-6 border-b-1 border-blueGray-300"/>
+				<hr className="mt-6 border-b-1 border-blueGray-300"/>
 
-				<h6 class="text-blueGray-400 text-2xl text-code mt-3 mb-6 font-bold uppercase">
+				<h6 className="text-blueGray-400 text-2xl text-code mt-3 mb-6 font-bold uppercase">
 				Preferences
 				</h6>
-				<div class="flex flex-wrap">
+				<div className="flex flex-wrap">
 
-				<div class="w-full lg:w-12/12 px-4">
-					<div class="relative w-full mb-3">
-					<label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" for="Gender">
+				<div className="w-full lg:w-12/12 px-4">
+					<div className="relative w-full mb-3">
+					<label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="Gender">
 						Gender
 					</label>
-						<div class="block pt-3 pb-2 space-x-4">
+						<div className="block pt-3 pb-2 space-x-4">
 							<label htmlFor='gender'>
 								<input
 								type="radio"
-								id="Gender"
+								id="genderMan"
 								name="Gender"
 								onChange={e => setGender(e.target.value)}
 								value="Man"
-								class="mr-2 text-black border-2 border-gray-300 focus:border-gray-300 focus:ring-black"
+								className="mr-2 text-black border-2 border-gray-300 focus:border-gray-300 focus:ring-black"
 								/>
 								Man
 							</label>
 							<label htmlFor='gender'>
 								<input
 								type="radio"
-								id="Gender"
+								id="genderWomen"
 								name="Gender"
 								onChange={e => setGender(e.target.value)}
 								value="Woman"
-								class="mr-2 text-black border-2 border-gray-300 focus:border-gray-300 focus:ring-black"
+								className="mr-2 text-black border-2 border-gray-300 focus:border-gray-300 focus:ring-black"
 								/>
 								Women
 							</label>
 							<label htmlFor='gender'>
 								<input
 								type="radio"
-								id="Gender"
+								id="genderMore"
 								name="Gender"
 								onChange={e => setGender(e.target.value)}
 								value="More"
-								class="mr-2 text-black border-2 border-gray-300 focus:border-gray-300 focus:ring-black"
+								className="mr-2 text-black border-2 border-gray-300 focus:border-gray-300 focus:ring-black"
 								/>
 								More
 							</label>
@@ -509,53 +509,53 @@ export default function Register({ currentUser, setCurrentUser }) {
 				</div>
 
 
-				<div class="w-full lg:w-12/12 px-4">
-					<div class="relative w-full mb-3">
-					<label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" for="lookingFor">
+				<div className="w-full lg:w-12/12 px-4">
+					<div className="relative w-full mb-3">
+					<label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="lookingFor">
 						I'm Looking For
 					</label>
-						<div class="block pt-3 pb-2 space-x-4">
+						<div className="block pt-3 pb-2 space-x-4">
 							<label htmlFor='lookingFor'>
 								<input
 								type="radio"
-								id="lookingFor"
+								id="lookingForMan"
 								name="lookingFor"
 								onChange={e => setLookingFor(e.target.value)}
 								value="Man"
-								class="mr-2 text-black border-2 border-gray-300 focus:border-gray-300 focus:ring-black"
+								className="mr-2 text-black border-2 border-gray-300 focus:border-gray-300 focus:ring-black"
 								/>
 								Man
 							</label>
 							<label htmlFor='lookingFor'>
 								<input
 								type="radio"
-								id="lookingFor"
+								id="lookingForWomen"
 								name="lookingFor"
 								onChange={e => setLookingFor(e.target.value)}
 								value="Woman"
-								class="mr-2 text-black border-2 border-gray-300 focus:border-gray-300 focus:ring-black"
+								className="mr-2 text-black border-2 border-gray-300 focus:border-gray-300 focus:ring-black"
 								/>
 								Women
 							</label>
 							<label htmlFor='lookingFor'>
 								<input
 								type="radio"
-								id="lookingFor"
+								id="lookingForFriends"
 								name="lookingFor"
 								onChange={e => setLookingFor(e.target.value)}
 								value="Friends"
-								class="mr-2 text-black border-2 border-gray-300 focus:border-gray-300 focus:ring-black"
+								className="mr-2 text-black border-2 border-gray-300 focus:border-gray-300 focus:ring-black"
 								/>
 								Friends
 							</label>
 							<label htmlFor='lookingFor'>
 								<input
 								type="radio"
-								id="lookingFor"
+								id="lookingForNoPreference"
 								name="lookingFor"
 								onChange={e => setLookingFor(e.target.value)}
 								value="No Preference"
-								class="mr-2 text-black border-2 border-gray-300 focus:border-gray-300 focus:ring-black"
+								className="mr-2 text-black border-2 border-gray-300 focus:border-gray-300 focus:ring-black"
 								/>
 								No Preference
 							</label>
@@ -565,12 +565,12 @@ export default function Register({ currentUser, setCurrentUser }) {
 
 				
 
-				<div class="w-full lg:w-12/12 px-4">
-					<div class="relative w-full mb-3">
-					<label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" for="favoriteLanguage">
+				<div className="w-full lg:w-12/12 px-4">
+					<div className="relative w-full mb-3">
+					<label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="favoriteLanguage">
 						Favorite Programming Language
 					</label>
-					<select class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-3/12 ease-linear transition-all duration-150" value={favoritePLanguage} required id="favoriteLanguage" name="favoriteLanguage" onChange={e => setFavoritePLanguage(e.target.value)}>
+					<select className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-3/12 ease-linear transition-all duration-150" value={favoritePLanguage} required id="favoriteLanguage" name="favoriteLanguage" onChange={e => setFavoritePLanguage(e.target.value)}>
 							<option value="Python">Python</option>
 							<option value="JavaScript">Javascript</option>
 							<option value="Java">Java</option>
@@ -591,47 +591,47 @@ export default function Register({ currentUser, setCurrentUser }) {
 			
 				</div>
 
-				<hr class="mt-6 border-b-1 border-blueGray-300"/>
+				<hr className="mt-6 border-b-1 border-blueGray-300"/>
 
-				<h6 class="text-blueGray-400 text-2xl text-code mt-3 mb-6 font-bold uppercase">
+				<h6 className="text-blueGray-400 text-2xl text-code mt-3 mb-6 font-bold uppercase">
 				About Me
 				</h6>
 
 
-				<div class="flex flex-wrap">
-				<div class="w-full lg:w-12/12 px-4">
-					<div class="relative w-full mb-3">
-					<textarea type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" rows="4"> Enter a bio and a pick up line here.</textarea>
+				<div className="flex flex-wrap">
+				<div className="w-full lg:w-12/12 px-4">
+					<div className="relative w-full mb-3">
+					<textarea type="text" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" rows="4"> Enter a bio and a pick up line here.</textarea>
 					</div>
 				</div>
 				</div>
 
 
-				<div class="w-full lg:w-12/12 px-4">
-					<div class="relative w-full mb-3">
-					<label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="profileimage">
+				<div className="w-full lg:w-12/12 px-4">
+					<div className="relative w-full mb-3">
+					<label className="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="profileimage">
 						Upload a profile picture
 					</label>
-					<input type="file" name='file' id="profileimage" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-4/12 ease-linear transition-all duration-150" value={city} onChange={uploadImage}/>
+					<input type="file" name='file' id="profileimage" className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-4/12 ease-linear transition-all duration-150" onChange={uploadImage}/>
 					</div>
 				</div>
 
 				<button
 				id="button"
 				type="submit"
-				class="w-2/12 px-6 py-3 mt-3 text-lg text-white transition-all duration-150 ease-linear rounded-lg shadow outline-none bg-pink-500 hover:bg-pink-600 hover:shadow-lg focus:outline-none"
+				className="w-2/12 px-6 py-3 mt-3 text-lg text-white transition-all duration-150 ease-linear rounded-lg shadow outline-none bg-pink-500 hover:bg-pink-600 hover:shadow-lg focus:outline-none"
 				>
 				Sign-Up
 			</button>
 
-				<hr class="mt-6 border-b-1 border-blueGray-300"/>
+				<hr className="mt-6 border-b-1 border-blueGray-300"/>
 
-				<h6 class="text-blueGray-400 text-2xl text-code mt-3 mb-6 font-bold uppercase">
+				<h6 className="text-blueGray-400 text-2xl text-code mt-3 mb-6 font-bold uppercase">
 				Profile Preview
 				</h6>
 
-				<div class="w-full lg:w-4/12 px-4">
-					<div class="relative w-full mb-3">
+				<div className="w-full lg:w-4/12 px-4">
+					<div className="relative w-full mb-3">
 					{ loading ? <img src={photo} style={{height: 250}}></img>: <img src={photo}></img> }
 					<p>{biography}</p>
 					</div>
