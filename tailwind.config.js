92c5578f7c2bfd0057ba5a4226d07cc52789771c
aimeepham14 <aimee.pham14@gmail.com>
2022-10-27 13:36:41 -0700

    added font colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,15 @@ module.exports = {
     extend: {
       fontFamily: {
         code: ['Source Code Pro', 'monospace'],
+        code2: ['Play', 'sans-serif'],
       },
     },
     textColor: {
       'primary': '#5BC288',
       'secondary': '#C37BB6',
       'yellow': '#FFD130',
-      'orange': '#CD866F'
+      'orange': '#CD866F',
+      'db': '#7fc9e9'
     },
   },
   plugins: [],
