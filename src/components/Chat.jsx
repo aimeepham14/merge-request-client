df9a4a19f294870b55cbda5fd04c8fa46558fb67
syorn96 <syorn2014@gmail.com>
2022-10-26 17:41:34 -0700

    chat box connects with backend

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,9 +1,22 @@
 import { useState } from 'react'
 
-export default function Chat() {
+export default function Chat({sortedMessages}) {
+    const displayMessages = sortedMessages.map((message,i) => {
+        return (
+            <div key={`${i}`}>
+                <div>
+                    <div>
+                        <img src={message.img_url} style={{height: 100}}></img>
+                    </div>
+                    <p>{message.name}</p>
+                </div>
+                <p>{message.content} sent:{message.timestamp}</p>
+            </div>
+        )
+    })
     return (
-        <div>
-            Display Chat Dialogue
-        </div>
+       <div>
+        {displayMessages}
+       </div>
     )
 }
--- src/components/RequestsChatDisplay.jsx
+++ src/components/RequestsChatDisplay.jsx
@@ -1,12 +1,79 @@
+import axios from 'axios'
+import { useEffect } from 'react'
 import { useState } from 'react'
 import Chat from './Chat'
 import ChatBox from './ChatBox'
 
-export default function RequestsChatDisplay() {
+export default function RequestsChatDisplay({currentUser}) {
+    const [userMessages, setUserMessages] = useState(null)
+    const [otherUserMessages, setOtherUserMessages] = useState(null)
+    const userId = currentUser.id
+    const clickedUserId = '6359802dfd3905ca2e1b4ffe'
+
+    // const clickedMatchId = clickedMatch?.id
+    const usersMessages = async() => {
+        try {
+            const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api-v1/messages/${userId}/${clickedUserId}`)
+        setUserMessages(response.data)
+        // console.log(response)
+        console.log(userMessages)
+        } catch(err){
+            console.warn(err)
+        }
+        
+    }
+    const otherUsersMessages = async() => {
+        try {
+            const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api-v1/messages/${clickedUserId}/${userId}`)
+        setOtherUserMessages(response.data)
+        // console.log(response)
+        console.log(userMessages)
+        } catch(err){
+            console.warn(err)
+        }
+        
+    }
+
+    useEffect(()=> {
+        usersMessages()
+        otherUsersMessages()
+    }, [])
+
+    const messages = []
+
+    userMessages?.forEach(message => {
+        const messageArray = {}
+        messageArray['name'] = currentUser?.firstName
+        messageArray['img_url'] = currentUser?.photo
+        messageArray['content'] = message.content
+        messageArray['timestamp'] = message.createdAt
+        messages.push(messageArray)
+        console.log(messages)
+        
+    })
+
+    // ADD CLICKED USER ONCE DONE
+    otherUserMessages?.forEach(message => {
+        const messageArray = {}
+        messageArray['name'] = 'Tyler'
+        messageArray['img_url'] = 'http://res.cloudinary.com/dspcnzoiy/image/upload/v1666809899/a2kqsqrg9t4auz6xc9yk.jpg'
+        messageArray['content'] = message.content
+        messageArray['timestamp'] = message.createdAt
+        messages.push(messageArray)
+        console.log(messages)
+        
+    })
+
+    const sortedMessages = messages?.sort((a,b) => a.timestamp.localeCompare(b.timestamp))
     return (
         <div>
-            <Chat/>
-            <ChatBox/>
+            <Chat 
+            sortedMessages={sortedMessages}/>
+            <ChatBox 
+            currentUser={currentUser}
+            usersMessages={usersMessages}
+            otherUsersMessages={otherUsersMessages}
+            />
         </div>
     )
 }
