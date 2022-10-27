8e58fcc9353038a7f62a9eac43f5b1510b93cf19
aimeepham14 <aimee.pham14@gmail.com>
2022-10-26 22:10:26 -0700

    still have bug on city

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -376,9 +376,10 @@ export default function Register({ currentUser, setCurrentUser }) {
 					<label class="block uppercase text-blueGray-600 text-xs font-bold mb-2" htmlFor="city">
 						City, State
 					</label>
-					<input type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value={city} id="city" onChange={e => setCity(e.target.value)}/>
+					<input type="text" class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150" value={city} id="city" onChange={e => setCity(e.target.value)} />
 					</div>
 				</div>
+
 
 				<div class="w-full lg:w-4/12 px-4">
 					<div class="relative w-full mb-3">
