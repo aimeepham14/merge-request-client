d686199d59ed0e543e55b06cd9e01b5cdc8e089b
tylerchan33 <tylerchan33@gmail.com>
2022-10-28 15:18:42 -0700

    adds modal for deleting profile

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,10 +1,16 @@
-export default function Modal () {
+export default function Modal ({ isVisible, onClose, children }) {
+    if ( !isVisible ) return null
+
+    const handleClose = (e) => {
+        if(e.target.id === "content") onClose()
+
+    }
     return (
-        <div className='fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center'>
-            <div className='w-[600px]'>
-                <button className="text-white text-xl"> X
+        <div className='fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center' id="content" onClick ={handleClose}>
+            <div className='w-[600px] flex flex-col'>
+                <button className="text-red text-xl place-self-end" onClick = {() => onClose()}> X
                 </button>
-                <div>
+                <div className="bg-white p-2 rounded"> {children}
                 </div>                
             </div>
         </div>
