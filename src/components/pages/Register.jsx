b5a5a9285c2d7ce691d72a2adcd3f6337ff52156
tylerchan33 <tylerchan33@gmail.com>
2022-10-28 10:14:44 -0700

    adds ternary for possible birth days on Register

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -424,39 +424,6 @@ export default function Register({ currentUser, setCurrentUser }) {
 					<label className="block uppercase text-m font-code text-db mb-2" for="birthDay">
 						Birth Day
 					</label>
-					{/* <select className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150 font-code" value={birthDay} required id="birthDay" onChange={e => setBirthDay(e.target.value)}>
-							<option value="1">1</option>
-							<option value="2">2</option>
-							<option value="3">3</option>
-							<option value="4">4</option>
-							<option value="5">5</option>
-							<option value="6">6</option>
-							<option value="7">7</option>
-							<option value="8">8</option>
-							<option value="9">9</option>
-							<option value="10">10</option>
-							<option value="11">11</option>
-							<option value="12">12</option>
-							<option value="13">13</option>
-							<option value="14">14</option>
-							<option value="15">15</option>
-							<option value="16">16</option>
-							<option value="17">17</option>
-							<option value="18">18</option>
-							<option value="19">19</option>
-							<option value="20">20</option>
-							<option value="21">21</option>
-							<option value="22">22</option>
-							<option value="23">23</option>
-							<option value="24">24</option>
-							<option value="25">25</option>
-							<option value="26">26</option>
-							<option value="27">27</option>
-							<option value="28">28</option>
-							<option value="29">29</option>
-							<option value="30">30</option>
-							<option value="31">31</option>
-					</select> */}
 					{birthMonth == 2 && birthYear % 4 == 0
                         ? 
                         <div>
