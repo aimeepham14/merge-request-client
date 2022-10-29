65c3f98808828c1c15e09f7c8c5adac51ab9927e
tylerchan33 <tylerchan33@gmail.com>
2022-10-29 13:31:48 -0700

    fixed distance

--- src/components/pages/Swipe.jsx
+++ src/components/pages/Swipe.jsx
@@ -153,11 +153,6 @@ export default function Swipe({currentUser}) {
         distances()
     },[distance])
 
-    
-
-    
-    
-
     useEffect(()=> {
         const getSelectedUser = async() => {
             try{
