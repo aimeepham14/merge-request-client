776f012f41cf631696e5143b3c9db10b2d97f049
aimeepham14 <aimee.pham14@gmail.com>
2022-10-30 03:43:08 -0700

    styled welcome page added icon

--- src/components/pages/Welcome.jsx
+++ src/components/pages/Welcome.jsx
@@ -1,8 +1,14 @@
 export default function Welcome() {
 	return (
 		<div className='bg-[#1C1C1C] h-screen flex flex-col'>
-			<h1 className= 'text-6xl mb-14 font-code text-primary pt-8 '>MERGE REQUEST</h1>
-			<img className=' sm:w-2/12 hero container mx-auto pb-10 ' src='https://i.imgur.com/mPiXyR3.png' ></img>
+			<h1 className= 'text-6xl mb-14 font-code text-primary pt-8 underline'>
+				MERGE
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-20 h-20 inline-block">
+  				<path fill-rule="evenodd" d="M14.447 3.027a.75.75 0 01.527.92l-4.5 16.5a.75.75 0 01-1.448-.394l4.5-16.5a.75.75 0 01.921-.526zM16.72 6.22a.75.75 0 011.06 0l5.25 5.25a.75.75 0 010 1.06l-5.25 5.25a.75.75 0 11-1.06-1.06L21.44 12l-4.72-4.72a.75.75 0 010-1.06zm-9.44 0a.75.75 0 010 1.06L2.56 12l4.72 4.72a.75.75 0 11-1.06 1.06L.97 12.53a.75.75 0 010-1.06l5.25-5.25a.75.75 0 011.06 0z" clip-rule="evenodd" />
+				</svg> 
+				REQUEST 
+			</h1>
+			<img className=' sm:w-2/12 hero w-40 mx-auto pb-10 ' src='https://i.imgur.com/mPiXyR3.png' ></img>
 			<h2 className= 'text-4xl mb-14 font-code text-secondary'>A dating app for developers to develop meaningful relationships</h2>
 		</div>
 
